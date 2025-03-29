5a1c617ea2f5f07f73b4943550524bd7c9577efb
hareem284 <sehrishandaleeb06@gmail.com>
2025-03-29 14:30:00 +0500

    Co-authored-by: SmritiJain123 <SmritiJain123@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from config import HF_API_KEY
-from PIL import Image
+from PIL import Image,ImageEnhance,ImageFilter
 from io import BytesIO
 import requests
 #
 API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-3-medium-diffusers"
+#defining post processing image
+def post_procesing_image(image):
+    #increasing the brightness
+    b_IMAGE=ImageEnhance.Brightness(image).enhance(1.2)#1.2 means 20% increase in brightness
+    c_image=ImageEnhance.Contrast(b_IMAGE).enhance(1.3)#1.3 means 30%
+    g_blur_image=c_image.filter(ImageFilter.GaussianBlur(radius=2))
+    return g_blur_image
+
+
+
+
+
+
+
+
+
 def generate_image_from_text(prompt:str):
     
     #putting headers
@@ -17,10 +33,10 @@ def generate_image_from_text(prompt:str):
     if 'image' in response.headers.get('Content-Type', ''):
             image = Image.open(BytesIO(response.content))
             return image
-    
-  
-    
-
+    else:
+        print("Could not get an image")
+        return None
+#making a starting function
 def start():
   #  Main loop for user interaction. Continuously prompts the user for a text description,generates an image via the API, displays it, and offers an option to save the image.
 
@@ -34,7 +50,18 @@ def start():
         else:
             print("generating image......")
             image =generate_image_from_text(prompt)
-            image.show()
+            if image!=  None:
+              image.show()
+              print("applying post procssesing effects")
+              p_image=post_procesing_image(image)
+              p_image.show()
+              save=input("do you want to save the image").strip().lower()
+              if save=="yes":
+                  f_name=input("please tell the file name which it should be saved in")
+                  p_image.save(f"{f_name}.png")
+
+            else:
+              break
 
 
 
